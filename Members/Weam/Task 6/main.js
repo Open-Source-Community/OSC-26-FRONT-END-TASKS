10bf0e0ee2860540e5be0c58be5ddb47ee844b20
WeaamNasser1 <weaamedu@gmail.com>
2026-03-10 19:13:57 +0200

    Task 6

---
+++ Members/Weam/Task 6/main.js
@@ -0,0 +1,15 @@
+const heroes = [
+    { id: 1, name: "Iron Man", power: 100, team: "Avengers" },
+    { id: 2, name: "Spider-man", power: 85, team: "Avengers" },
+    { id: 3, name: "Batman", power: 90, team: "Justice League" },
+    { id: 4, name: "Wonder Woman", power: 95, team: "Justice League" },
+    { id: 5, name: "Black Widow", power: 80, team: "Avengers" },
+    { id: 6, name: "Superman", power: 100, team: "Justice League" },
+    { id: 7, name: "Wolverine", power: 92, team: "X-Men" },
+    { id: 8, name: "Thor", power: 98, team: "Avengers" },
+    { id: 9, name: "Flash", power: 94, team: "Justice League" },
+    { id: 10, name: "Storm", power: 88, team: "X-Men" },
+];
+console.table(heroes);
+const Avenger=heroes.filter(hero => hero.team==="Avengers");
+console.table(Avenger);
