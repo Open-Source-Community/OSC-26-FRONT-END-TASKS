ed9e60ac32dfaada02835a629642625a71c58d34
MalakMohamed89 <zxcv94566@gmail.com>
2026-05-11 14:54:40 +0300

    task-1

---
+++ Members/Malak-Farouk/frontend.tasks/OSC.js
@@ -0,0 +1,28 @@
+let employees = [
+    { name: "Ahmed", age: 35, job: "Backend", salary: 8000 },
+    { name: "Ayman", age: 25, job: "Flutter", salary: 6000 },
+    { name: "Mahmoud", age: 30, job: "Frontend", salary: 7000 },
+    { name: "Ola", age: 37, job: "Backend", salary: 10000 },
+    { name: "Maged", age: 23, job: "Backend", salary: 8500 },
+    { name: "Hamdy", age: 28, job: "Frontend", salary: 5000 },
+    { name: "Yasmine", age: 28, job: "Backend", salary: 10000 },
+    { name: "Gamal", age: 26, job: "Flutter", salary: 8000 },
+    { name: "Rashed", age: 33, job: "Backend", salary: 10000 },
+    { name: "Hoda", age: 25, job: "Backend", salary: 10000 }
+];
+
+// let result = employees.filter(emp =>
+//     emp.age == 23 ||
+//     (emp.age == 28 && emp.name.startsWith("Y")) ||
+//     emp.age == 26 ||
+//     (emp.age == 25 &&emp.name.startsWith("H"))
+// );
+// console.table(result);
+ 
+// let result = employees.filter(emp =>
+//     ["M", "Y", "G", "H"].includes(emp.name[0]) &&
+//     ["d", "e", "l", "a"].includes(
+//         emp.name[emp.name.length - 1]
+//     ) &&emp.name!=="Mahmoud"
+// );
+// console.table(result);
